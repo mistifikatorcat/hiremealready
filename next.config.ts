f91a7d86e7b96a50f509f2a234b1e6ef36c427c1
Daniel Evgrafov <danyaevgrafov@gmail.com>
2025-04-30 20:15:28 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   optimizeFonts: false,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
