b2f04e182bf2d1889611a79aafeefeaa8aa46ed7
Daniel Evgrafov <danyaevgrafov@gmail.com>
2025-04-30 20:07:44 +0300

    more eslint

--- eslintrc.js
+++ eslintrc.js
@@ -1,10 +1,9 @@
 // .eslintrc.js
 module.exports = {
- 
+
     rules: {
 
-      'react/no-unescaped-entities': 'off',
-
+        'react/no-unescaped-entities': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
     },
-  };
-  
+};
